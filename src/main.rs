26b5fe5ebc7a8697f520c7e7958037cfe5379593
Mahmud Ahmad <duelwith_mahmud@live.com>
2024-03-07 22:39:23 -0500

    simple config getter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+fn config() -> (String, String) {
+	let args: Vec<String> = env::args().collect();
+	dbg!(args);
+
+	return ("/Users/mahmudahmad/Downloads/tmp.yml".to_string(), "null".to_string());
+}
+
 fn main() {
-    println!("Hello, world!");
+	let input_file: String;
+	let n:          String;
+
+	(input_file, n) = config();
+	println!("input_file: {input_file}, n: {n}");
 }
